3a3eb7191f104f6575acc7caf45a2388338e6b2b
Muhammad Omar Ibrahim <momarib7@gmail.com>
2026-02-18 14:46:23 +0700

    add implementation of Node class for Linked List

---
+++ Pertemuan_4/praktikum4-II.py
@@ -0,0 +1,10 @@
+#================================================
+# Implementasi Dasar : Node pada Linked List
+# Nama : Muhammad Omar Ibrahim
+# NIM : J0403251093
+#================================================
+
+class Node:
+    def __init__(self):
+        self.data = data #menyimpan nilai data
+        self.next = None #pointer ke node berikutnya
