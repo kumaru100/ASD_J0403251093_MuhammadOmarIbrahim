286c51201e2d6312dc11516c74b98c76d815fb76
kumaru100 <momarib7@gmail.com>
2026-02-26 06:51:43 +0000

    benerin indentasi, masih skeleton

--- Pertemuan_4/praktikum4.tugas.py
+++ Pertemuan_4/praktikum4.tugas.py
@@ -3,58 +3,58 @@
 # ==========================================================
 
 class Node:
- def __init__(self, no, nama, servis):
- self.no = no
- self.nama = nama
- self.servis = servis
- self.next = None
+    def __init__(self, no, nama, servis):
+        self.no = no
+        self.nama = nama
+        self.servis = servis
+        self.next = None
 
 class QueueBengkel:
- def __init__(self):
- self.front = None
- self.rear = None
+    def __init__(self):
+        self.front = None
+        self.rear = None
 
- def enqueue(self, no, nama, servis):
+    def enqueue(self, no, nama, servis):
  # TODO: Tambahkan data ke antrian
- pass
+        pass
 
- def dequeue(self):
+    def dequeue(self):
  # TODO: Layani pelanggan terdepan
- pass
+        pass
 
- def tampilkan(self):
+    def tampilkan(self):
  # TODO: Tampilkan seluruh antrian
- pass
+        pass
 
 
 def main():
- q = QueueBengkel()
- while True:
- print("\n=== Sistem Antrian Bengkel ===")
- print("1. Tambah Pelanggan")
- print("2. Layani Pelanggan")
- print("3. Lihat Antrian")
- print("4. Keluar")
+    q = QueueBengkel()
+    while True:
+        print("\n=== Sistem Antrian Bengkel ===")
+        print("1. Tambah Pelanggan")
+        print("2. Layani Pelanggan")
+        print("3. Lihat Antrian")
+        print("4. Keluar")
 
- pilih = input("Pilih menu: ")
+        pilih = input("Pilih menu: ")
 
- if pilih == "1":
- no = input("No Antrian : ")
- nama = input("Nama : ")
- servis = input("Servis : ")
- q.enqueue(no, nama, servis)
+        if pilih == "1":
+            no = input("No Antrian : ")
+            nama = input("Nama : ")
+            servis = input("Servis : ")
+            q.enqueue(no, nama, servis)
 
- elif pilih == "2":
- q.dequeue()
+        elif pilih == "2":
+            q.dequeue()
   
- elif pilih == "3":
- q.tampilkan()
+        elif pilih == "3":
+            q.tampilkan()
 
- elif pilih == "4":
- break
+        elif pilih == "4":
+            break
 
- else:
- print("Pilihan tidak valid")
+        else:
+            print("Pilihan tidak valid")
 
 if __name__ == "__main__":
  main()
