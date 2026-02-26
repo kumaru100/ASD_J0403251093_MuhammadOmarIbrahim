33f8ab239301e0a3513e986a7f469df2e7afdba4
Muhammad Omar Ibrahim <momarib7@gmail.com>
2026-02-27 05:21:39 +0700

    tugas prak 4 kelar

--- Pertemuan_4/praktikum4.tugas.py
+++ Pertemuan_4/praktikum4.tugas.py
@@ -14,17 +14,50 @@ class QueueBengkel:
         self.front = None
         self.rear = None
 
+    def is_empty(self):
+        #Ketika queue kosong maka front = rear = none
+        return self.front is None
+
     def enqueue(self, no, nama, servis):
- # TODO: Tambahkan data ke antrian
-        pass
+        # TODO: Tambahkan data ke antrian
+        nodeBaru = Node(no, nama, servis)
+        # gunakan pemanggilan method agar kondisi dievaluasi dengan benar
+        if self.is_empty():
+            self.front = nodeBaru
+            self.rear  = nodeBaru
+            return
+        
+        self.rear.next = nodeBaru
+        self.rear = nodeBaru
+    
 
     def dequeue(self):
  # TODO: Layani pelanggan terdepan
-        pass
+        if self.is_empty():
+            print("Antrian Kosong, Tidak ada motor yang diperbaiki !")
+            return None
+        
+                #Lihat data bagian front, simpan di variabel yang akan dihapus
+        nodeDilayani = self.front
+
+        #Geser pointer front ke next node
+        self.front = self.front.next
+
+        #Jika front menjadi none (data antrian terakhit yang dilayani), maka front = rear = none
+        if self.front is None:
+            self.rear = None
+
+        return nodeDilayani
 
     def tampilkan(self):
  # TODO: Tampilkan seluruh antrian
-        pass
+        print("Daftar Antrian Bengkel Motor (Front -> Rear) : ")
+        current = self.front
+        no = 1
+        while current is not None:
+            print(f"{no}. {current.no}, {current.nama} = {current.servis}")
+            current = current.next
+            no += 1
 
 
 def main():
@@ -48,6 +81,7 @@ def main():
             q.dequeue()
   
         elif pilih == "3":
+            print("\n")
             q.tampilkan()
 
         elif pilih == "4":
@@ -57,4 +91,4 @@ def main():
             print("Pilihan tidak valid")
 
 if __name__ == "__main__":
- main()
+    main()
